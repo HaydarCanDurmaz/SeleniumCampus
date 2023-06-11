f9e7a0a3efcee4e6c43a1502d7af58343cd4676a
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-06-11 23:58:22 +0300

    StudentLogin , Passwoed

--- src/TestCampus/StudentLogin.java
+++ src/TestCampus/StudentLogin.java
@@ -2,12 +2,20 @@ package TestCampus;
 
 import GWD.BaseDriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class StudentLogin extends BaseDriver {
     @Test
     public void StudentLogin()
     {
         driver.get("https://test.mersys.io/");
+
+        WebElement Login = driver.findElement(By.id("mat-input-0"));
+        Login.sendKeys("S5_11C");
+
+        WebElement Password = driver.findElement(By.id("mat-input-1"));
+        Password.sendKeys("12345");
 
 
 
