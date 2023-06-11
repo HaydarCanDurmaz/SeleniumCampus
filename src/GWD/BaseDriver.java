dcb5d7441ff0eeb96c9cd7c3a532dc7646885b1d
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-06-11 11:07:54 +0300

    MyFunc Creat it

--- src/GWD/BaseDriver.java
+++ src/GWD/BaseDriver.java
@@ -1,5 +1,26 @@
 package GWD;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseDriver {
+
+
+
+ public static void DriverStart()
+ {
+     WebDriver driver = new ChromeDriver();
+ }
+
+
+
+ public static void WaitClose()
+ {
+
+
+     MyFunc.Wait(3);
+     driver.quit;
+ }
+
 
 }
---
+++ src/GWD/MyFunc.java
@@ -0,0 +1,14 @@
+package GWD;
+
+public class MyFunc {
+
+
+    public static void Wait(int sn)
+    {
+        try {
+            Thread.sleep(1000*sn);
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
