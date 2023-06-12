dfee0fe0392d4953fd3ceffe473e93715d5f926c
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-06-13 00:38:52 +0300

    TeacherSteps , Passwoed

--- src/TestCampus/TeacherSteps.java
+++ src/TestCampus/TeacherSteps.java
@@ -1,4 +1,23 @@
 package TestCampus;
 
-public class TeacherSteps {
+import GWD.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TeacherSteps  extends BaseDriver {
+    public void Test(){
+        driver.get("https://test.mersys.io/");
+
+        WebElement username = driver.findElement(By.id("mat-input-0"));
+        username.sendKeys("teacher_3");
+
+        WebElement Password = driver.findElement(By.id("mat-input-1"));
+        Password.sendKeys("T12345");
+
+        WebElement Login = driver.findElement(By.cssSelector(""));
+
+
+
+        BekleKapat();
+    }
 }
